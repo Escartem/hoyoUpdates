fa1938cc7e5b5cab2ee22b1e20a50d8290213084
Escartem <escartem.yt@gmail.com>
2025-05-05 21:13:36 +0000

    fixes

--- components/global-warning.tsx
+++ components/global-warning.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { OctagonAlert, Sparkles, X } from "lucide-react";
+import { OctagonAlert, X } from "lucide-react";
 import { useState } from "react";
 
 export default function GlobalWarning() {
@@ -12,7 +12,7 @@ export default function GlobalWarning() {
         <div className={`absolute z-[900] w-full h-[30px] bg-red-400 flex items-center justify-center p-2 transition ${!open && "translate-y-[-100%] opacity-0"}`}>
             <div className="w-full flex text-sm md:text-xl items-center">
                 <OctagonAlert className={`${textColor} mr-2`} />
-                <p className={textColor}>Genshin and ZZZ won't receive latest updates for now due to a change in the update mechanism &lt;(＿　＿)&gt;</p>
+                <p className={textColor}>Genshin and ZZZ won&apos;t receive latest updates for now due to a change in the update mechanism &lt;(＿　＿)&gt;</p>
             </div>
             <X onClick={() => {setOpen(false)}} className={`${textColor} hover:cursor-pointer`} />
         </div>
